1415813baddbe652b55ba3171a9324f523a73138
Boseong Kwon <kwonline21@gmail.com>
2024-09-11 01:38:17 +0900

    회원가입 API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import UsersRouter from './routes/users.router.js';
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/api', [UsersRouter]);
 
 app.listen(PORT, () => {
   console.log(`The server opened with ${PORT}ports`);
---
+++ src/routes/users.router.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+import bcrypt from 'bcrypt';
+
+const router = express.Router();
+
+// sign-up API
+router.post('/sign-up', async (req, res, next) => {
+  const { id, password, repeatPassword, name } = req.body;
+
+  // ID 조건
+  const idValidity = /^[a-z0-9]+$/;
+  if (!idValidity.test(id)) {
+    return res
+      .status(400)
+      .json({ message: '아이디는 영어 소문자와 숫자로만 입력해 주세요.' });
+  }
+
+  // 비밀번호 조건
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: '비밀번호는 최소 6자 이상으로 입력해 주세요.' });
+  }
+
+  // 비밀번호 재확인
+  if (password !== repeatPassword) {
+    return res
+      .status(400)
+      .json({ message: '재확인 비밀번호가 일치하지 않습니다.' });
+  }
+
+  // 존재하는 아이디 확인
+  const isExistUser = await prisma.users.findFirst({
+    where: { id },
+  });
+
+  if (isExistUser) {
+    return res.status(409).json({ message: '이미 존재하는 아이디입니다.' });
+  }
+
+  // password 암호화 & user 생성
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.users.create({
+    data: {
+      id,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // 가입성공
+  return res
+    .status(201)
+    .json({ message: '회원가입이 완료되었습니다.', data: user });
+});
+
+export default router;
