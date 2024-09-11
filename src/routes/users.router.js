19299ffcd0561f5e94042faae48ddfa0adf73e5d
Boseong Kwon <kwonline21@gmail.com>
2024-09-11 10:45:15 +0900

    로그인 API

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -53,6 +54,25 @@ router.post('/sign-up', async (req, res, next) => {
   return res
     .status(201)
     .json({ message: '회원가입이 완료되었습니다.', data: user });
+});
+
+// sign-in API
+router.post('/sign-in', async (req, res, next) => {
+  const { id, password } = req.body;
+
+  const user = await prisma.users.findFirst({ where: { id } });
+
+  if (!user) {
+    return res.status(401).json({ message: '존재하지 않는 아이디입니다.' });
+  }
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+  }
+
+  const token = jwt.sign({ userId: user.userId }, 'custom-secret-key');
+
+  res.cookie('authorization', `Bearer ${token}`);
+  return res.status(200).json({ message: '로그인에 성공하였습니다.' });
 });
 
 export default router;
