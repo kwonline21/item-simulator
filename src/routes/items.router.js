3b4a5e75b8ea9e54f33950bd7cfa25ef1a421904
Boseong Kwon <kwonline21@gmail.com>
2024-09-11 17:48:23 +0900

    아이템 목록 조회 / 상세 조회 API

--- src/routes/items.router.js
+++ src/routes/items.router.js
@@ -31,4 +31,41 @@ router.post('/items', async (req, res, next) => {
     .json({ message: '아이템 생성이 완료되었습니다.', data: item });
 });
 
+// 아이템 목록 조회 API
+router.get('/items', async (req, res, next) => {
+  const items = await prisma.items.findMany({
+    select: {
+      itemCode: true,
+      itemName: true,
+      itemPrice: true,
+    },
+  });
+
+  return res.status(200).json({ data: items });
+});
+
+// 아이템 상세 조회 API
+router.get('/items/:itemCode', async (req, res, next) => {
+  const { itemCode } = req.params;
+
+  const isExistsItem = await prisma.items.findFirst({
+    where: { itemCode: +itemCode },
+  });
+  if (!isExistsItem) {
+    return res.status(401).json({ message: '존재하지 않는 아이템입니다.' });
+  }
+
+  const item = await prisma.items.findFirst({
+    where: { itemCode: +itemCode },
+    select: {
+      itemCode: true,
+      itemName: true,
+      itemStat: true,
+      itemPrice: true,
+    },
+  });
+
+  return res.status(200).json({ data: item });
+});
+
 export default router;
