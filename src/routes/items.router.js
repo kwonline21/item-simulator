22388088e7729000157a3892e84b5dd7637651ba
Boseong Kwon <kwonline21@gmail.com>
2024-09-11 19:42:21 +0900

    아이템 수정 API
    아이템 생성 API + stat 조건

--- src/routes/items.router.js
+++ src/routes/items.router.js
@@ -8,6 +8,15 @@ const router = express.Router();
 router.post('/items', async (req, res, next) => {
   const { itemName, itemStat, itemPrice } = req.body;
 
+  if (
+    typeof itemStat.health !== 'number' ||
+    typeof itemStat.power !== 'number'
+  ) {
+    return res
+      .status(404)
+      .json({ message: '능력치는 숫자로만 설정할 수 있습니다.' });
+  }
+
   const isExistItem = await prisma.items.findFirst({
     where: { itemName },
   });
@@ -66,6 +75,44 @@ router.get('/items/:itemCode', async (req, res, next) => {
   });
 
   return res.status(200).json({ data: item });
+});
+
+// 아이템 수정 API
+router.put('/items/:itemCode', async (req, res, next) => {
+  const { itemCode } = req.params;
+  const { itemName, itemStat } = req.body;
+
+  console.log(itemStat.health);
+  if (
+    typeof itemStat.health !== 'number' ||
+    typeof itemStat.power !== 'number'
+  ) {
+    return res
+      .status(404)
+      .json({ message: '능력치는 숫자로만 변경할 수 있습니다.' });
+  }
+
+  const item = await prisma.items.findUnique({
+    where: {
+      itemCode: +itemCode,
+    },
+  });
+
+  if (!item) {
+    return res.status(404).json({ message: '존재하지 않는 아이템입니다.' });
+  }
+
+  await prisma.items.update({
+    data: {
+      itemName,
+      itemStat,
+    },
+    where: {
+      itemCode: +itemCode,
+    },
+  });
+
+  return res.status(200).json({ date: '아이템이 수정되었습니다.' });
 });
 
 export default router;
